1a4845f8e2d0f4b786b25cc6a05b044ab71f0d90
unknown <247934357@qq.com>
2020-01-29 11:24:25 +0800

    '新增防抖和节流'

--- interviewer/js/test.js
+++ interviewer/js/test.js
@@ -1,1 +1,13 @@
-console.log(a);
+function b(func, wait = 500) {
+    let timer;
+    return function () {
+        if (timer) clearTimeout(timer);
+        timer = setTimeout(() => {
+            func.apply(this, arguments)
+        }, wait)
+    }
+}
+
+(b(function () {
+    console.log(1);
+}))()
