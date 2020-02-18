7fc71e0ca272d51f457f992e926b74b14d67b82a
unknown <247934357@qq.com>
2020-02-18 22:21:53 +0800

    '优化面试的一些简单算法题，使用md记录'

---
+++ interviewer/js/readMe.md
@@ -0,0 +1,4 @@
+算法.md是记录自己面试和总结的一些会用到的算法题
+使用的是doctoc
+1.npm i doctoc -g //install 简写 i
+2.假如你的markdown文件在work/demo.md文件下，只需要cd work 切换到当前文件目录下，执行doctoc demo.md文件，即可在文档中自动生成目录。
--- interviewer/js/test.js
+++ interviewer/js/test.js
@@ -1,13 +1,13 @@
-function b(func, wait = 500) {
-    let timer;
-    return function () {
-        if (timer) clearTimeout(timer);
-        timer = setTimeout(() => {
-            func.apply(this, arguments)
-        }, wait)
-    }
-}
-
-(b(function () {
-    console.log(1);
-}))()
+// function b(func, wait = 500) {
+//     let timer;
+//     return function () {
+//         if (timer) clearTimeout(timer);
+//         timer = setTimeout(() => {
+//             func.apply(this, arguments)
+//         }, wait)
+//     }
+// }
+//
+// (b(function () {
+//     console.log(1);
+// }))()
