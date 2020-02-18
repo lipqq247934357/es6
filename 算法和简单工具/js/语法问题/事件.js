7bfe3020e53a965a2afa3cc6a87527cc3e2b964b
unknown <247934357@qq.com>
2020-02-18 18:41:34 +0800

    '修改事件机制代码，增加注释'

--- 算法和简单工具/js/语法问题/事件.js
+++ 算法和简单工具/js/语法问题/事件.js
@@ -1,3 +1,22 @@
+/**
+ *
+ * 事件注册触发机制
+ *  主要原理：
+ *      event对象作为一个数据结构存储所以注册事件
+ *      结构：event:{
+ *              funcName:[func1,func2]
+ *                  ...
+ *          }
+ *
+ *       on：
+ *          给event对象添加方法，方法名为key
+ *       emit:
+ *          到event对象中找对应的方法，然后执行
+ *       off:
+ *          将event对象指定key的数组置空
+ */
+
+
 class EventEmitter {
     constructor() {
         this.event = {};
