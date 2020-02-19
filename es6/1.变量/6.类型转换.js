02db6adfeb9fff228851bffed5d5c630fdf6aa8b
unknown <247934357@qq.com>
2020-02-19 13:10:21 +0800

    '增加类型转换代码'

---
+++ es6/1.变量/6.类型转换.js
@@ -0,0 +1,19 @@
+/**
+ *
+ * 类型转换
+ *
+ */
+
+function booleanTest() {
+    console.log('undefined:', Boolean(undefined));
+    console.log('null:', Boolean(null));
+    console.log('number:', Boolean(0), Boolean(NaN), Boolean(1));
+    console.log('string:', Boolean(''), Boolean('11'));
+    console.log('object:', Boolean({}));
+}
+
+// booleanTest();
+
+function numberTest() {
+
+}
