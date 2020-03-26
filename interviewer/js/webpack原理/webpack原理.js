2b6e5ac44c9a3b499e859832a3e6698e04e7dbbf
李鹏 <247934357@qq.com>
2020-03-26 10:49:44 +0800

    增加webpack原理，修改删除啊一些代码

---
+++ interviewer/js/webpack原理/webpack原理.js
@@ -0,0 +1,71 @@
+(function (modules) {
+    function require(id) { //🌟
+        const [fn, mapping] = modules[id];
+
+        function localRequire(name) { //⏰
+            return require(mapping[name]); //🌟
+        }
+
+        const module = {exports: {}};
+
+        fn(localRequire, module, module.exports);
+
+        return module.exports;
+    }
+
+    require(0);
+})({
+    0: [
+        function (require, module, exports) {
+            "use strict";
+
+            var _message = require("./message.js");
+
+            var _message2 = _interopRequireDefault(_message);
+
+            function _interopRequireDefault(obj) {
+                return obj && obj.__esModule ? obj : {default: obj};
+            }
+
+            console.log(_message2.default);
+        },
+        {"./message.js": 1},
+    ], 1: [
+        function (require, module, exports) {
+            "use strict";
+
+            Object.defineProperty(exports, "__esModule", {
+                value: true
+            });
+
+            var _name = require("./name.js");
+
+            exports.default = "hello " + _name.name + "!";
+        },
+        {"./name.js": 2},
+    ], 2: [
+        function (require, module, exports) {
+            "use strict";
+
+            Object.defineProperty(exports, "__esModule", {
+                value: true
+            });
+            var name = exports.name = 'world';
+        },
+        {},
+    ],
+});
+
+/**
+ *
+ * 以上js代码为一个demo打包生成的js代码
+ *
+ * 具体实现原理参考：https://github.com/chinanf-boy/minipack-explain
+ *  主要原理是：
+ *      从入口处依次解析找出所有的模块和依赖，然后把他们组成一个对象，key为模块id，value是一个数组，第一项是使用类似commonjs模块的函数，第二部分是当前模块依赖组成的一个对象
+ *      然后把它包装成自治性函数,写一个require方法，通过require函数执行入口函数，从而执行所有的代码
+ *      通过require执行函数，通过module.exports返回结果
+ *      中间涉及到使用babel将es6转化成ast，转换成es5的过程
+ *      还有模块找到他的依赖再解析依赖的过程
+ *
+ */
--- 算法和简单工具/js/js作用域题/js作用域.js
+++ 算法和简单工具/js/js作用域题/js作用域.js
@@ -1,24 +1,42 @@
+/**
+ *
+ *    this的取值取决于调用这个方法的对象，是一个执行期间才会生成的东西
+ *    绑定规则：
+ *  1.由new 调用？绑定到新创建的对象
+ *  2.由call或者apply调用？绑定到指定的对象
+ *  3.由某个对象调用，绑定到那个对象
+ *  4.默认：在严格模式下绑定到undefined，否则绑定的全局window
+ *  5.箭头函数：箭头函数会继承外层函数的调用的this绑定如果在全局下就是window或者undefined
+ *
+ *
+ *  这种题的复杂性主要体现在：
+ *      this+函数作用域+箭头函数+异步
+ */
+
 window.a = 200;
 
-function func () {
+function func() {
     let a = 300;
     let obj = {
         a: 11,
         b: function () {
-            console.log(this.a,'b --> this.a')
+            (function () {
+                console.log(this.a, 'b--> 闭包函数');
+            })(); // 第5个规则
+            console.log(this.a, 'b --> this.a');
             console.log(a, 'b --> a')
         },
         c: function () {
             setTimeout(() => {
-                console.log(this.a,'c --> this.a')
+                console.log(this.a, 'c --> this.a');
                 console.log(a, 'c --> a')
             })
         },
-        d: function(){
-            obj.c.call({a:500});
+        d: function () {
+            obj.c.call({a: 500});
         }
-    }
-    return [obj.b,obj.c,obj.d];
+    };
+    return [obj.b, obj.c, obj.d];
 }
 
 let res = func();
