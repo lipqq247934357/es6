01b8d5ea14ddaf056924f015d5edf76b408bb716
lipeng <247934357@qq.com>
2021-06-15 14:25:10 +0800

    update: 更新temp

--- temp/0.js
+++
@@ -1,6 +0,0 @@
-/**
- * node直接运行一些工具
- * 
- */
-
-let a = require('../algorithm_tools/js/实用小工具/ip');
--- temp/5.mjs
+++
@@ -1,5 +0,0 @@
-// app.mjs
-import { addTwo } from './4.mjs';
-
-// Prints: 6
-console.log(addTwo(4));
