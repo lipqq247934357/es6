a8a824f1fd222969b81ecafd41c9684ef49e9d73
lipeng <247934357@qq.com>
2021-08-09 16:45:23 +0800

    feat: new

---
+++ es5/js对象函数类/new.js
@@ -0,0 +1,34 @@
+// 构造函数如果返回基本数据类型，则返回值无意义
+function Test1(name) {
+    this.name = name
+    return 1
+}
+const t1 = new Test1('yck')
+console.log('t1.name:' + t1.name) // 'yck'
+// 构造函数如果返回值为对象，那么这个返回值会被正常使用
+function Test2(name) {
+    this.name = name
+    console.log(this) // Test { name: 'yck' }
+    return { age: 26 }
+}
+const t2 = new Test2('yck')
+console.log(t2) // { age: 26 }
+console.log(t2.name) // 'undefined'
+// 实现new 运算符
+const Person = function () {
+    this.name = 'aa';
+}
+Person.prototype.age = 28;
+const funcNew = (func, ...args) => {
+    let obj = {};
+    obj.__proto__ = func.prototype;
+    const result = func.apply(obj, args);
+    return result instanceof Object ? result : obj;
+}
+
+const p = funcNew(Person);
+console.log(p.name);
+console.log(p.age);
+
+
+
