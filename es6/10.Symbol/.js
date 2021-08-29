171b575cb3c1622530b62dc199d7121ad195c008
lipeng <247934357@qq.com>
2021-08-29 20:18:58 +0800

    feat: symbol

--- es6/10.Symbol/.js
+++ es6/10.Symbol/.js
@@ -1,4 +1,5 @@
-//
+// 
 let myExports = {};
+console.log(Object.prototype.toString.call(myExports));
 Object.defineProperty(myExports, Symbol.toStringTag, { value: "Module" });
 console.log(Object.prototype.toString.call(myExports)); //[object Module]
