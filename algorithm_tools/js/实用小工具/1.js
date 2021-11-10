87f152084756fd75a7b7c685dca32b5346844f54
lipeng <247934357@qq.com>
2021-11-10 15:40:59 +0800

    refactor: 1

---
+++ algorithm_tools/js/实用小工具/1.js
@@ -0,0 +1,8 @@
+import formatDuration, { formatDuration4Min } from "../utils.js";
+
+console.log('formatDuration(1000)', formatDuration(1000));
+console.log('formatDuration(23)', formatDuration(23));
+console.log('formatDuration(222)', formatDuration(222));
+console.log('formatDuration(100000)', formatDuration(100000));
+console.log('formatDuration4Min(1200)', formatDuration4Min(1200));
+console.log('formatDuration4Min(100000)', formatDuration4Min(100000));
