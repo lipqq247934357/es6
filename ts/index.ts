a8cfb5ce7f89c3265c456c4c597da73a68eb9e83
lipeng <247934357@qq.com>
2021-11-15 15:41:32 +0800

    refactor: dir-structor

--- ts/index.ts
+++ ts/index.ts
@@ -1,5 +1,5 @@
 // import "./ts/basic/1basicType";
 // import "./ts/basic/2enum";
 // import "./ts/basic/3interface";
-import "./ts/basic/4function";
+import "./basic/4function";
 export default {};
